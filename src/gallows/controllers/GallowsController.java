d2456abd267cf85daefd76911644dfa54d9148c1
itp1797 <antipov11.11@gmail.com>
2017-07-15 19:01:19 +0300

    Adaptive view

--- src/gallows/controllers/GallowsController.java
+++ src/gallows/controllers/GallowsController.java
@@ -1,18 +1,18 @@
 package gallows.controllers;
 
+import gallows.model.Game;
 import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import gallows.model.Game;
-
+import javafx.scene.layout.*;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -25,8 +25,10 @@ public class GallowsController implements Initializable {
     public AnchorPane pane;
     public Pane keyBoardPane;
     public ImageView gallowsPicture;
-    public Pane gameLetters;
+    public AnchorPane gameLetters;
     public PieChart pieStat;
+    public HBox boxWithLetters;
+    public GridPane keyboardTable;
     private GallowsLetter[] letters;
     private Image[] gallows;
     private Game game;
@@ -51,26 +53,36 @@ public class GallowsController implements Initializable {
                 new PieChart.Data("Поражение", 0)
         ));
         pieStat.getData().forEach(data -> data.nameProperty().bind(
-                Bindings.concat( data.getName(), " ", data.pieValueProperty())
+                Bindings.concat(data.getName(), " ", data.pieValueProperty())
         ));
     }
 
     private void clearField() {
-        keyBoardPane.getChildren().clear();
-        gameLetters.getChildren().clear();
+        keyboardTable.getChildren().clear();
+        keyboardTable.getColumnConstraints().clear();
+        keyboardTable.getRowConstraints().clear();
+        boxWithLetters.getChildren().clear();
     }
 
     private void prepareKeyboard() {
         String alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".toUpperCase();
 
         final int widthButtonsTable = 10;
+        final int heightButtonsTable = (int) Math.ceil(1.0 * alphabet.length() / widthButtonsTable);
 
-        final int BUTTON_WIDTH = 50;
-        final int BUTTON_HEIGHT = 30;
-        final double MARGIN_X = 1.2;
-        final double MARGIN_Y = 1.2;
-        final double START_X = (keyBoardPane.getPrefWidth() - (BUTTON_WIDTH * widthButtonsTable * MARGIN_X)) / 2.0;
-        final double START_Y = 10;
+        for (int i = 0; i < widthButtonsTable; i++) {
+            ColumnConstraints colConst = new ColumnConstraints();
+            colConst.setPercentWidth(100.0 / widthButtonsTable);
+            keyboardTable.getColumnConstraints().add(colConst);
+        }
+        for (int i = 0; i < heightButtonsTable; i++) {
+            RowConstraints rowConst = new RowConstraints();
+            rowConst.setPercentHeight(100.0 / heightButtonsTable);
+            keyboardTable.getRowConstraints().add(rowConst);
+        }
+
+        final int BUTTON_WIDTH = 55;
+        final int BUTTON_HEIGHT = 35;
 
         for (int i = 0; i < widthButtonsTable; i++) {
             int widthLine = widthButtonsTable <= alphabet.length() ? widthButtonsTable : alphabet.length();
@@ -78,25 +90,25 @@ public class GallowsController implements Initializable {
                 Button key = new Button(String.valueOf(alphabet.charAt(j)));
                 key.setPrefWidth(BUTTON_WIDTH);
                 key.setPrefHeight(BUTTON_HEIGHT);
-                key.setLayoutX(START_X + j * MARGIN_X * BUTTON_WIDTH);
-                key.setLayoutY(START_Y + i * BUTTON_HEIGHT * MARGIN_Y);
                 key.setFocusTraversable(false);
-                keyBoardPane.getChildren().add(key);
                 key.setOnMouseClicked(event -> guessLetter(((Button) event.getSource())));
+                GridPane.setValignment(key, VPos.CENTER);
+                GridPane.setHalignment(key, HPos.CENTER);
+                GridPane.setFillWidth(key, true);
+                keyboardTable.add(key, j, i);
             }
             alphabet = alphabet.substring(widthLine, alphabet.length());
         }
     }
 
     private void prepareLetters() {
+        final double LETTERS_MARGIN = 5;
         letters = new GallowsLetter[game.getWordLength()];
         final double LETTERS_SIZE = 50;
-        final double LETTERS_MARGIN = 1.2;
-        final double LETTERS_OFFSET = (gameLetters.getPrefWidth() - LETTERS_SIZE * LETTERS_MARGIN * letters.length) / 2.0;
+        boxWithLetters.setSpacing(LETTERS_MARGIN);
         for (int i = 0; i < letters.length; i++) {
             letters[i] = new GallowsLetter(LETTERS_SIZE, LETTERS_SIZE);
-            letters[i].setLayoutX(LETTERS_OFFSET + LETTERS_SIZE * LETTERS_MARGIN * i);
-            gameLetters.getChildren().add(letters[i]);
+            boxWithLetters.getChildren().add(letters[i]);
         }
     }
 
